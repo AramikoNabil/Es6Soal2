ad54e648a6310ff4c02ce1eb1c2534a9983d220a
sleepy81k <nabilnico@gmail.com>
2020-09-15 21:56:43 +0700

    first

---
+++ Soal2.js
@@ -0,0 +1,17 @@
+// Mengubah Aray biasa menjadi Array Destructuring
+
+const arr = [90, 102, 1231, 3112, 801, 812];
+
+var [q, w, e, r, t, y, u] = arr;
+
+console.log(q);
+
+console.log(w);
+
+console.log(e);
+
+console.log(r);
+
+console.log(y);
+
+
